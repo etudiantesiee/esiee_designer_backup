94906105273fc6f0f7ef188fc16ad9200c6dd240
lti <landry.tiehi@gmail.com>
2016-10-06 23:20:32 +0200

    Ajout test unitaire Engine

---
+++ src/test/java/fr/esiee/pic/esieedesigner/engine/ExecutionEngineTest.java
@@ -0,0 +1,33 @@
+package fr.esiee.pic.esieedesigner.engine;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExecutionEngineTest {
+	
+	@Test
+	public void testSingleton() {
+		ExecutionEngine i1 = ExecutionEngine.getInstance();
+		ExecutionEngine i2 = ExecutionEngine.getInstance();
+		assertEquals(i1, i2);
+	}
+	
+	@Test
+	public void testAffichageAvecGrille() {
+		ExecutionEngine i1 = ExecutionEngine.getInstance();
+		i1.afficher(true);
+	}
+	
+	@Test
+	public void testAffichageSansGrille() {
+		ExecutionEngine i1 = ExecutionEngine.getInstance();
+		i1.afficher(false);
+	}
+	
+	@Test
+	public void testMasquer() {
+		ExecutionEngine i1 = ExecutionEngine.getInstance();
+		i1.masquer();
+	}
+
+}
