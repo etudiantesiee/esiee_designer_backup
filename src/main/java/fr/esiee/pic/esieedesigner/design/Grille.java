aa689adbfa8f640cd9cfc12c15efda5d79bb484f
lti <landry.tiehi@gmail.com>
2016-10-11 16:21:49 +0200

    Ajout de la partie verticale de la grille

--- src/main/java/fr/esiee/pic/esieedesigner/design/Grille.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/Grille.java
@@ -7,15 +7,14 @@ import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 /**
  * Grille pour faciliter les calculs
  * 
- * La grille est vu comme un repère avec 
- * 	- une origine : POINT_BORDURE_HAUT_DROIT
- * 	- Une unité de longueur : uniteLongueur
- * 	- Une unité de largeur : uniteLargeur
+ * La grille est vu comme un repère avec - une origine :
+ * POINT_BORDURE_HAUT_DROIT - Une unité de longueur : uniteLongueur - Une unité
+ * de largeur : uniteLargeur
  * 
  * On pose que la longueur correspond au côté horizontal (l'axe des abscisse)
  * 
  * @author etudiant
- *
+ * 
  */
 public class Grille extends CreateurDeForme {
 
@@ -23,27 +22,27 @@ public class Grille extends CreateurDeForme {
 	 * Point situé en haut à gauche de la brodure supérieure
 	 */
 	private static final Point POINT_BORDURE_HAUT_DROIT = new Point(0, 0);
-	
+
 	/**
 	 * Longeur de l'écran dans lequel sera afficher le cadre
 	 */
 	private final double longueurEcran;
-	
+
 	/**
 	 * Largeur de l'écran dans lequel sera afficher le cadre
 	 */
 	private final double largeurEcran;
-	
+
 	/**
 	 * Longueur de chaque portion formant la grille
 	 */
 	private final double uniteLongueur;
-	
+
 	/**
 	 * Largeur de chaque portion formant la grille
 	 */
 	private final double uniteLargeur;
-	
+
 	/**
 	 * Constructeur
 	 * 
@@ -52,33 +51,66 @@ public class Grille extends CreateurDeForme {
 	 * @param uniteLongueur
 	 * @param uniteLargeur
 	 */
-	public Grille(double longueurEcran, double largeurEcran, double uniteLongueur, double uniteLargeur) {
+	public Grille(double longueurEcran, double largeurEcran,
+			double uniteLongueur, double uniteLargeur) {
 		this.uniteLongueur = uniteLongueur;
 		this.uniteLargeur = uniteLargeur;
 		this.longueurEcran = longueurEcran;
 		this.largeurEcran = largeurEcran;
 	}
-	
+
 	@Override
 	public void dessiner() {
+		dessinerBarresVerticales();
+		dessinerBarresHorizontales();
+	}
+
+	/**
+	 * Dessin des barres verticales de la grille
+	 */
+	private void dessinerBarresVerticales() {
 		// Ajout des points sur l'axe des abscisses
-		
+
 		Point pointCourant = POINT_BORDURE_HAUT_DROIT;
 		double xPointCourant = pointCourant.getX();
 		double yPointCourant = pointCourant.getY();
-		
+
 		while (xPointCourant <= longueurEcran) {
 			double xSymetriquePointCourant = xPointCourant;
-			double ySymetriquePointCourant = yPointCourant + longueurEcran;
-			
-			Point symetriquePointCourant = new Point(xSymetriquePointCourant, ySymetriquePointCourant);
-			demarrerNouveauDessinAvecDesPoints()
-			.ajouter(pointCourant)
-			.ajouter(symetriquePointCourant)
-			.colorier(Couleur.GRIS);
-			
+			double ySymetriquePointCourant = yPointCourant + largeurEcran;
+
+			Point symetriquePointCourant = new Point(xSymetriquePointCourant,
+					ySymetriquePointCourant);
+			demarrerNouveauDessinAvecDesPoints().ajouter(pointCourant)
+					.ajouter(symetriquePointCourant).colorier(Couleur.GRIS);
+
 			xPointCourant = xPointCourant + this.uniteLongueur;
-			
+
+			pointCourant = new Point(xPointCourant, yPointCourant);
+		}
+	}
+	
+	/**
+	 * Dessin des barres horizontales de la grille
+	 */
+	private void dessinerBarresHorizontales() {
+		// Ajout des points sur l'axe des abscisses
+
+		Point pointCourant = POINT_BORDURE_HAUT_DROIT;
+		double xPointCourant = pointCourant.getX();
+		double yPointCourant = pointCourant.getY();
+
+		while (yPointCourant <= largeurEcran) {
+			double xSymetriquePointCourant = xPointCourant + longueurEcran;
+			double ySymetriquePointCourant = yPointCourant;
+
+			Point symetriquePointCourant = new Point(xSymetriquePointCourant,
+					ySymetriquePointCourant);
+			demarrerNouveauDessinAvecDesPoints().ajouter(pointCourant)
+					.ajouter(symetriquePointCourant).colorier(Couleur.GRIS);
+
+			yPointCourant = yPointCourant + this.uniteLargeur;
+
 			pointCourant = new Point(xPointCourant, yPointCourant);
 		}
 	}
