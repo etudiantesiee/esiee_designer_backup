df81697d0952ddb3cef1c55770eafc74d4378eb9
r.poon <jean.dupont@mail.fr>
2016-11-30 10:18:47 +0100

    G2 ajout visage

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
@@ -14,27 +14,27 @@ import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 
 public class DessinsGroupe2 extends CreateurDeForme {
 	
-private static final Point Origin= new Point(60*25,5*25);
+private static final Point origin= new Point(60*25,5*25);
 
 
 	@Override
 	public void dessiner() {
 		/*cheveux*/
-		Point a= new Point(Origin.getX()+50,0);
-		Point b= new Point(Origin.getX()+100,25);
-		Point c= new Point(Origin.getX()+150,75);
-		Point d= new Point(Origin.getX()+150,200);//pic1
-		Point e= new Point(Origin.getX()+125,150);
-		Point f= new Point(Origin.getX()+125,100);
-		Point g= new Point(Origin.getX()+125,200);
-		Point h= new Point(Origin.getX()+100,150);
-		Point i= new Point(Origin.getX()+100,125);
-		Point j= new Point(Origin.getX()+100,175);
-		Point k= new Point(Origin.getX()+75,125);
-		Point l= new Point(Origin.getX()+75,100);
-		Point m= new Point(Origin.getX()+50,75);
-		Point n= new Point(Origin.getX()+25,75);
-		Point o= new Point(Origin.getX()+0,50);
+		Point a= new Point(origin.getX()+50,origin.getY()+0);
+		Point b= new Point(origin.getX()+100,origin.getY()+25);
+		Point c= new Point(origin.getX()+150,origin.getY()+75);
+		Point d= new Point(origin.getX()+150,origin.getY()+200);//pic1
+		Point e= new Point(origin.getX()+125,origin.getY()+150);
+		Point f= new Point(origin.getX()+125,origin.getY()+100);
+		Point g= new Point(origin.getX()+125,origin.getY()+200);
+		Point h= new Point(origin.getX()+100,origin.getY()+150);
+		Point i= new Point(origin.getX()+100,origin.getY()+125);
+		Point j= new Point(origin.getX()+100,origin.getY()+175);
+		Point k= new Point(origin.getX()+75,origin.getY()+125);
+		Point l= new Point(origin.getX()+75,origin.getY()+100);
+		Point m= new Point(origin.getX()+50,origin.getY()+75);
+		Point n= new Point(origin.getX()+25,origin.getY()+75);
+		Point o= new Point(origin.getX()+0,origin.getY()+50);
 		
 		/*dessin cheveux*/
 		demarrerNouveauDessinAvecDesPoints().ajouter(a)
@@ -67,12 +67,29 @@ private static final Point Origin= new Point(60*25,5*25);
 		.ajouter(getSymetrie(b))
 		.ajouter(getSymetrie(a));
 		
+		/*visage*/
+		Point p= new Point(origin.getX()+75,origin.getY()+200);
+		Point q= new Point(origin.getX()+25,origin.getY()+225);
+		
+		/*dessin visage*/
+		demarrerNouveauDessinAvecDesPoints().ajouter(p).ajouter(j);
+		demarrerNouveauDessinAvecDesPoints().ajouter(p).ajouter(q);
+		demarrerNouveauDessinAvecDesPoints().ajouter(q).ajouter(getSymetrie(q));
+		demarrerNouveauDessinAvecDesPoints().ajouter(getSymetrie(q)).ajouter(getSymetrie(p));
+		demarrerNouveauDessinAvecDesPoints().ajouter(getSymetrie(p)).ajouter(getSymetrie(j));
+		
+		/*bouche*/
+		Point s= new Point(origin.getX()+75,origin.getY()+150);
+		Point t= new Point(origin.getX()+75,origin.getY()+175);
+		Point u= new Point(origin.getX()+25,origin.getY()+200);
+		
+		
 		}
 	
 	
 	
 	private Point getSymetrie(Point point){
-		return new Point(2*Origin.getX()-point.getX(), point.getY());
+		return new Point(2*origin.getX()-point.getX(), point.getY());
 		
 	}
 
