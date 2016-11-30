ea48daddd012a93d8dd6db75e8af47f42b0b0850
f.quemper <frederic.quemper@edu.esiee.fr>
2016-11-30 09:41:13 +0100

    Ajout pied groupe2

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe2.java
@@ -13,11 +13,31 @@ import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 
 
 public class DessinsGroupe2 extends CreateurDeForme {
+	/**
+	 * Pas de dessin horizontale
+	 */
+	private static final double UNITE_HORIZONTALE = 50;
+	private static final double OFFSET = ;
+	/**
+	 * Pas de dessin verticale
+	 */
+	private static final double UNITE_VERTICALE = 50;
 	
-private static final Point Origin= new Point(350,50);
+	private static final Point Origin= new Point(350,50);
 
 	@Override
 	public void dessiner() {
+		
+		 Point Talon_droite = new Point(7*UNITE_HORIZONTALE,UNITE_VERTICALE*14);
+		 Point BoutPied_droite = new Point(9*UNITE_HORIZONTALE,UNITE_VERTICALE*14);
+		 Point HautPied_droite = new Point(8*UNITE_HORIZONTALE,UNITE_VERTICALE*13);
+		 
+		 
+		 demarrerNouveauDessinAvecDesPoints()
+      		.ajouter(Talon_droite)
+		    .ajouter(BoutPied_droite)
+		    .ajouter(HautPied_droite);
+		 
 		
 	}
 	
