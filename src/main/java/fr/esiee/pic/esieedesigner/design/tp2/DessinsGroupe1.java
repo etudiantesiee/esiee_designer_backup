a99d0c304fd9022655919ed93d3d5a6e9be8200b
a.najar <amine.najar@mail.fr>
2016-11-30 09:39:32 +0100

    lunette

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
@@ -7,8 +7,9 @@ import fr.esiee.pic.esieedesigner.api.tools.CreateurDeForme;
 /**
  * Le groupe 1 ajoutera ses dessins dans cette classe.
  * 
- * @author etudiant
- *
+ * @author ady & diane
+ *         florian
+ *         amineoos &pradhox
  */
 public class DessinsGroupe1 extends CreateurDeForme {
 
@@ -17,6 +18,9 @@ public class DessinsGroupe1 extends CreateurDeForme {
 		// Ajout vos dessins ici
 		// Pensez à désactiver les dessins de la démo 
 		// en commentant initDemo dans le méthode init()de la classe fr.esiee.pic.esieedesigner.ui.scene.EcranPrincipal
+	  
+		
+		//menton
 	  Point mentona = new Point(75,175);
 	  Point mentonb = new Point(150,250);
 	  Point mentonc = new Point(200,250);
@@ -29,8 +33,40 @@ public class DessinsGroupe1 extends CreateurDeForme {
       		.ajouter(mentond)
       		.nePasRelierLesPointsExtreme();
 
+	   // tete
+      Point tetea = new Point(75,175);
+      Point teteb = new Point(75,75);
+      Point tetec = new Point(125,50);
+      Point teted = new Point(175,75);
+      Point tetee = new Point(225,50);
+      Point tetef = new Point(275,75);
+      Point teteg = new Point(275,175);
+       
+        	demarrerNouveauDessinAvecDesPoints()
+        		.ajouter(tetea)
+        		.ajouter(teteb)
+        		.ajouter(tetec)
+        		.ajouter(teted)
+        		.ajouter(tetee)
+        		.ajouter(tetef)
+        		.ajouter(teteg)
+        		.nePasRelierLesPointsExtreme();
+      	
+       // lunette rayban aviator
+        Point lunettea = new Point(75,100);
+        Point lunetteb = new Point(125,125);
+        Point lunettec = new Point(175,100);
+        Point lunetted = new Point(225,125);
+        Point lunettee = new Point(275,100);
 	  
-	  
+       		demarrerNouveauDessinAvecDesPoints()
+       			.ajouter(lunettea)
+       			.ajouter(lunetteb)
+       			.ajouter(lunettec)
+       			.ajouter(lunetted)
+       			.ajouter(lunettee)
+       			.nePasRelierLesPointsExtreme();
+              
 	}
   
 }
