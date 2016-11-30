c79368a04dd372434e03e747ddbd8fc5245b1f4a
f.silva <florian.silva@edu.esiee.fr>
2016-11-30 11:42:12 +0100

    décalage cheveux

--- src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
+++ src/main/java/fr/esiee/pic/esieedesigner/design/tp2/DessinsGroupe1.java
@@ -210,16 +210,16 @@ public class DessinsGroupe1 extends CreateurDeForme{
 	 * dessiner cheveux
 	 */
 	public void dessinerCheveux(){
-		Point basGaucheTete = new Point(2*X,7*X);
-        Point gaucheTete2 = new Point(X,3*X);
-        Point gaucheTete3 = new Point(X,X);
-        Point gaucheTete4 = new Point(2*X,0);
-        Point hautDroite = new Point(12*X,0);
-        Point hautDroite2 = new Point(13*X,X);
-        Point hautDroite3 = new Point(13*X,3*X);
-        Point hautDroite4 = new Point(12*X,7*X);
-        Point basDroite = new Point(11*X,7*X);
-        Point basGauche = new Point(3*X,7*X);     
+		Point basGaucheTete = new Point(32*X,7*X);
+        Point gaucheTete2 = new Point(31*X,3*X);
+        Point gaucheTete3 = new Point(31*X,X);
+        Point gaucheTete4 = new Point(32*X,0);
+        Point hautDroite = new Point(42*X,0);
+        Point hautDroite2 = new Point(43*X,X);
+        Point hautDroite3 = new Point(43*X,3*X);
+        Point hautDroite4 = new Point(42*X,7*X);
+        Point basDroite = new Point(41*X,7*X);
+        Point basGauche = new Point(33*X,7*X);     
 
         
         demarrerNouveauDessinAvecDesPoints()
